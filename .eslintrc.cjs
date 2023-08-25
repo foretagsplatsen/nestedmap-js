302c3d83198587220ea19fc1c2201b4316e66846
Damien Cassou <damien@cassou.me>
2023-08-25 14:52:15 +0200

    Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  root: true,
+  extends: [
+    "plugin:import/recommended",
+    "plugin:prettier/recommended",
+    "plugin:promise/recommended",
+  ],
+  env: {
+    es2022: true,
+  },
+};
